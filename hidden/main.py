efbc7001c81f88028113ff851ed39b4d2b5b997a
Teva RIOU <teva@yusofleet.com>
2020-08-06 16:27:12 +0200

    fix: script exit

--- hidden/main.py
+++ hidden/main.py
@@ -6,31 +6,22 @@ import time
 import math
 
 
-async def read_me(url, start):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            if re.search('^\S*$', await resp.text()):
-                stop = time.time()
-                print(
-                    f"{url}\n"
-                    f"{await resp.text()}"
-                    f"Runtime: "
-                    f"{math.floor((stop - start) / 60)} minutes "
-                    f"{math.floor((stop - start) % 60)} seconds"
-                )
-                exit(0)
-
-
-async def scrape(response, start):
+async def scrape(response):
     soup = BeautifulSoup(await response.text(), features="html.parser")
-    for a_tag in reversed(soup.find_all('a', href=True)[1:]):
-        url = str(response.url) + a_tag['href']
-        if a_tag['href'] == 'README':
-            await read_me(url, start)
+    for ref in reversed(soup.find_all('a', href=True)[1:]):
+        url = str(response.url) + ref['href']
+        if ref['href'] == 'README':
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as resp:
+                    if re.search('^\S*$', await resp.text()):
+                        print(f"{url}\n{await resp.text()}")
+                        return True
         else:
             async with aiohttp.ClientSession() as session:
                 async with session.get(url) as resp:
-                    await scrape(resp, start)
+                    if await scrape(resp):
+                        return True
+    return False
 
 
 async def run():
@@ -38,8 +29,13 @@ async def run():
     url = "http://192.168.0.35/.hidden/"
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as resp:
-            await scrape(resp, start)
-
+            await scrape(resp)
+    stop = time.time()
+    print(
+        f"Runtime: "
+        f"{math.floor((stop - start) / 60)} minutes "
+        f"{math.floor((stop - start) % 60)} seconds"
+    )
 
 if __name__ == '__main__':
     asyncio.run(run())
